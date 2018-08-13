9e9781af42565196f56bd5c9b29c39e935707fac
Nicholas Valletta <Inchworm333@users.noreply.github.com>
2018-08-13 11:16:05 -0400

    Create Gmail-to-Inbox.user.js

---
+++ Gmail-to-Inbox.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Gmail to Inbox
+// @namespace    https://github.com/Inchworm333
+// @version      0.1
+// @description  Change the Gmail button back to Inbox (thanks google for removing that)
+// @author       Inchworm333
+// @match        https://www.google.com
+// @match        https://www.google.com/webhp?authuser=*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var gmailButton = document.querySelectorAll('[data-pid="23"]')[0];
+
+    var gmailURL = new URL(gmailButton.href);
+
+    gmailButton.href = 'https://inbox.google.com/u/' + gmailURL.searchParams.get("authuser");
+
+    gmailButton.text = "Inbox";
+
+})();
